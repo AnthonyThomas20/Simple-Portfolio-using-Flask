7af233cbf03e03fc232f914526a7309934c7adde
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2021-05-13 08:28:57 +0530

    Update app.py

--- flask-profile/app.py
+++ flask-profile/app.py
@@ -1,4 +1,13 @@
+import gspread
 from flask import Flask, render_template
+
+gc = gspread.service_account(filename='flask-profile.json')
+sh = gc.open('flask-profile')
+
+shProfile = sh.get_worksheet(0)
+shContacts = sh.get_worksheet(1)
+shContacts.append_row('John Doe', 'johndoe@gmail.com', 'Hello')
+
 app = Flask(__name__)
 
 @app.route('/')
